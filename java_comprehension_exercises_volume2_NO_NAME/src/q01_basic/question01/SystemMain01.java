596157e4317b3e4aeeb235d41c602e55fc219644
呉作棟（WUZuodong） <WUZuodong@CST0100088>
2026-04-28 16:15:28 +0900

    0428 q01_basic.question02まで完了
    理解出来た点
    クラスとオブジェクトの概念
    フィールドとメソッド
    
    不明な点、調査中
    アクセス制御
    カプセル化について

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question01/SystemMain01.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question01/SystemMain01.java
@@ -16,13 +16,27 @@ age:36
 rank:2
 *****************
 
- * 
+
+
+/**
+ * システム実行
  */
 public class SystemMain01 {
-
 	public static void main(String[] args) {
-		//TODO ここから実装する
+		System.out.println("---SHOW DATA---");
 
+		Member member = new Member();
+
+		member.name = "Miura Manabu";
+		member.age = 24;
+		member.rank = 1;
+
+		member.showMember();
+
+		member.name = "Sato Kensuke";
+		member.age = 36;
+		member.rank = 2;
+
+		member.showMember();
 	}
-
 }
--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question02/SystemMain02.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question02/SystemMain02.java
@@ -1,9 +1,28 @@
 package q01_basic.question02;
 
+/**
+ * 系统的主要执行类
+ */
 public class SystemMain02 {
-
 	public static void main(String[] args) {
-		//TODO ここから実装する
+		System.out.println("---SHOW DATA---");
 
+		// 创建一个 Member 实例
+		Member member = new Member();
+
+		// 赋予初始值 (注意：这里变成了调用 set 方法)
+		member.setName("Miura Manabu");
+		member.setAge(24);
+		member.setRank(1);
+
+		// 显示信息
+		member.showMember();
+
+		// 测试升级功能
+		System.out.println("\n---RANK UP 发生！---");
+		member.rankUp(); // 调用升级方法
+
+		// 再次显示信息，你会发现 rank 变成了 2
+		member.showMember();
 	}
 }
