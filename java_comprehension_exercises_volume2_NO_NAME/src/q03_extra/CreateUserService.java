c341af2bafcac80ca1e77b52570794cce65fee4b
呉作棟（WUZuodong） <WUZuodong@CST0100088>
2026-05-01 14:22:31 +0900

    docのプッシュです

--- java_comprehension_exercises_volume2_NO_NAME/src/q03_extra/CreateUserService.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q03_extra/CreateUserService.java
@@ -2,13 +2,13 @@ package q03_extra;
 
 /**
  * MemberStorageにアクセスし、ユーザ登録処理を行う
- */
-class CreateUserService {
-	/**
-	 * memberStorage Memberクラスのリスト一覧
-	 */
-	private MemberStorage memberStorage;
-
-	//TODO ここから実装する
-
-}
+// */
+//class CreateUserService {
+//	/**
+//	 * memberStorage Memberクラスのリスト一覧
+//	 */
+//	private MemberStorage memberStorage;
+//
+//	//TODO ここから実装する
+//
+//}
